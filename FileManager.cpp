48b50d8b00125e2ccc4d50ff74a5c68ea9ad25e6
Yuun Bum Lim <dla0267@gmail.com>
2018-08-06 15:37:42 -0700

    filemanager cpp file added

---
+++ FileManager.cpp
@@ -0,0 +1,24 @@
+#include "fileManager.h"
+
+FileManager::FileManager() {}
+
+FileManager::~FileManager() {}
+    
+void FileManager::CreateFile(string fileName, int numBlocks) {
+    // create new INode
+    
+    // iNode iNode_ = new iNode()
+    
+}
+    
+void FileManager::AddFile(string fileName, int numBlocks) {
+
+}
+    
+void FileManager::DeleteFile(string fileName) {
+
+}
+
+void FileManager::DeleteBlocks(string fileName, int numBlocks) {
+
+}
