d844abe0e698a60b0fbceaf4e27584e71db34615
ccomai92 <ccomai92@gmail.com>
2018-08-06 15:05:38 -0700

    command

--- Kernel.cpp
+++ Kernel.cpp
@@ -5,8 +5,21 @@ using namespace std;
 
 int main() {
     // while loop taking commands 
+    string command; 
+    
     while(true) {
-        
+        cin >> command;
+        if (command == "FM") {
+
+        } else if (command == "NF") {
+
+        } else if (command == "MF") {
+
+        } else if (command == "DF") {
+
+        } else if (command == "DB") {
+            
+        }
     }
 
     return 0; 
