694da60662c50b11a507337f238e16838602b217
ccomai92 <ccomai92@gmail.com>
2018-08-06 15:18:50 -0700

    kernel done

--- Kernel.cpp
+++ Kernel.cpp
@@ -1,24 +1,48 @@
 
 #include <string> 
 #include <iostream> 
+#include "fileManager.h"
 using namespace std;
 
 int main() {
     // while loop taking commands 
     string command; 
+    FileManager* fileManager = nullptr; 
     
+
     while(true) {
+        cout << ">> "; 
         cin >> command;
         if (command == "FM") {
+            if (fileManager != nullptr) {
+                cerr << "File Structure already opened" << endl;  
+            } else {
+                fileManager = new FileManager(); 
+            }
+        } else { 
+            if (fileManager == nullptr) {
+                cerr << "File Structure has not been built yet" << endl;
+            } else {
+                string fileName; 
+                int blockNum; 
 
-        } else if (command == "NF") {
+                if (command == "NF") {
+                    cin >> fileName >> blockNum; 
+                    fileManager.createFile(fileName, blockNum); 
+                } else if (command == "MF") {
+                    cin >> fileName >> blockNum; 
+                    fileManager.addBlock(fileName, blockNum); 
 
-        } else if (command == "MF") {
+                } else if (command == "DF") {
+                    cin >> fileName; 
+                    fileManager.deleteFile(fileName); 
 
-        } else if (command == "DF") {
+                } else if (command == "DB") {
+                    cin >> fileName >> blockNum; 
+                    fileManager.deleteBlock(fileName, blockNum); 
 
-        } else if (command == "DB") {
-            
+                }
+            }
         }
     }
 
