d9d9eab90b1db1278575d7551aca15b787b2f5c5
ccomai92 <ccomai92@gmail.com>
2018-08-10 14:26:07 -0700

    aaaa

--- iNode.cpp
+++ iNode.cpp
@@ -62,12 +62,13 @@ int INode::getSize() {
     return this->size; 
 }
 
+// reference: 
+// https://stackoverflow.com/questions/997946/how-to-get-current-time-and-date-in-c
 // Get current date/time, format is YYYY-MM-DD.HH:mm:ss
 const std::string INode::currentDateTime() {
-    time_t     now = time(0);
     struct tm  tstruct;
     char       buf[80];
-    tstruct = *localtime(&now);
+    tstruct = *localtime(&this->aTime);
     // Visit http://en.cppreference.com/w/cpp/chrono/c/strftime
     // for more information about date/time format
     strftime(buf, sizeof(buf), "%Y-%m-%d.%X", &tstruct);
